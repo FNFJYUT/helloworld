627b2d044c9144d4507eca40c0911a2e9553196c
hasee <hasee@DESKTOP-FBDU83D>
2018-11-22 09:46:44 +0800

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,12 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		Object[] object = new Object[1];
-		Object obj1 = new Object();
-		System.out.println(obj1);
-		System.out.println(object);
-
-		Object[] object2 = new Object[0];
-		System.out.println(object2);
+		System.out.println("fuck you");
+		System.out.println("update git");
 	}
 }
