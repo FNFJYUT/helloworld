bb789010aeeea1ca5924c00c72b0bf8a061ae5e4
hasee <hasee@DESKTOP-FBDU83D>
2018-11-22 09:38:00 +0800

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Object[] object = new Object[1];
+		Object obj1 = new Object();
+		System.out.println(obj1);
+		System.out.println(object);
+
+		Object[] object2 = new Object[0];
+		System.out.println(object2);
+	}
+}
